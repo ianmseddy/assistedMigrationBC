5e28be6f5030933d3b23fa45571cac6a3d528a69
Ian <ianmseddy@gmail.com>
2020-03-12 08:31:04 -0700

    edits

--- R/generateProvenanceTable.R
+++ R/generateProvenanceTable.R
@@ -4,7 +4,7 @@
 #' BECvarfut_plantation, BECvar_seed, and HTp_pred. If not provided, this will return a table where
 #' every species is planted wherever possible, according to ecoregionDT
 #' @param ecoregionCol the column name in ecoregion to match with transferTable - this matching is hardcoded
-#' @param futureBEC the projected BEC zones. The zones MUST correspond to ecoregionKey and cohortData
+#' @param projectedBEC the projected BEC zones. The zones MUST correspond to ecoregionKey and cohortData
 #' @param ecoregionKey a table for mapping ecoregion codes to the BEC zone/subzone/variant
 #' column used by transferTable
 #' @param sppEquiv the LandR sppEquivalency table for matchign species names
@@ -12,52 +12,49 @@
 #' @param method experimental - this will change how particular species are prioritized for planting
 #' @return a provenance table object to be used with LandR_reforestation
 #' @export
-generateBCProvenanceTable <- function(transferTable = NULL, ecoregionCol, ecoregionKey, futureBEC,
+generateBCProvenanceTable <- function(transferTable = NULL, ecoregionCol, ecoregionKey, projectedBEC,
                                       method = 'default', sppEquiv, sppEquivCol) {
 
   browser()
   transferTable <- copy(transferTable)
-  if (!is.null(transferTable)) {
-    #due to the factorization of the zsv column in BEC zone data,
-    splitUpZones <- strsplit(x = as.character(ecoregionKey[, get(ecoregionCol)]), split = "-")
-    newCol <- lapply(splitUpZones, FUN = function(x){
-      newX <- gsub(x, pattern = "^NA", replacement = "") %>%
-        paste(., collapse = "")
-    }) %>%
-      unlist(.)
 
-    ecoregionKey$newCol <- newCol
-    spp <- c('BC_Forestry', eval(sppEquivCol))
-    sppEquiv <- sppEquiv[, .SD, .SDcols = spp]
-    transferTable <- transferTable[sppEquiv, on = c("species" = "BC_Forestry")]
-    transferTable[, species := NULL]
-    setnames(transferTable, eval(sppEquivCol), 'speciesCode')
+  #due to the factorization of the zsv column in BEC zone data,
+  splitUpZones <- strsplit(x = as.character(ecoregionKey[, get(ecoregionCol)]), split = "-")
+  newCol <- lapply(splitUpZones, FUN = function(x){
+    newX <- gsub(x, pattern = "^NA", replacement = "") %>%
+      paste(., collapse = "")
+  }) %>%
+    unlist(.)
 
-    setkey(ecoregionKey, newCol)
-    setkey(transferTable, BECvarfut_plantation)
+  ecoregionKey$newCol <- newCol
+  spp <- c('BC_Forestry', eval(sppEquivCol))
+  sppEquiv <- sppEquiv[, .SD, .SDcols = spp]
+  transferTable <- transferTable[sppEquiv, on = c("species" = "BC_Forestry")]
+  transferTable[, species := NULL]
+  setnames(transferTable, eval(sppEquivCol), 'speciesCode')
 
-    if (method == "default") {
+  setkey(ecoregionKey, newCol)
+  setkey(transferTable, BECvarfut_plantation)
 
-      #subset each ecoregionGroup/species by the minimum height reduction (ie max when expressed as proportion) among provenances
-      optimalProvenance <- transferTable[, score := rank(HTp_pred, ties.method = 'random'), by = .(BECvarfut_plantation, speciesCode)]# %>%
-        .[, best := max(score), .(BECvarfut_plantation, speciesCode)] %>%
-        .[score == best,]
+  if (method == "default") {
 
-      optimalProvenance <- optimalProvenance[ecoregionKey, on = c('BECvarfut_plantation' = 'newCol'), allow.cartesian = TRUE]
-      setnames(optimalProvenance, old = "BECvar_seed", "Provenance")
+    #subset each ecoregionGroup/species by the minimum height reduction (ie max when expressed as proportion) among provenances
+    optimalProvenance <- transferTable[, score := rank(HTp_pred, ties.method = 'random'), by = .(BECvarfut_plantation, speciesCode)]# %>%
+    .[, best := max(score), .(BECvarfut_plantation, speciesCode)] %>%
+      .[score == best,]
 
-      provenanceTable <- optimalProvenance[, .(ecoregionGroup, Provenance, speciesCode)]
+    optimalProvenance <- optimalProvenance[ecoregionKey, on = c('BECvarfut_plantation' = 'newCol'), allow.cartesian = TRUE]
+    setnames(optimalProvenance, old = "BECvar_seed", "Provenance")
 
-    } else if (method == "Elizabeth's other ideas") {
-      #implement Elizabeth's other ideas here
-    } else {
-      stop("unrecognized method")
-    }
-    #When time is available, scenarios that aren't relevant to CS - e.g. no hardwood reforestation, no migration, etc
+    provenanceTable <- optimalProvenance[, .(ecoregionGroup, Provenance, speciesCode)]
+
+  } else if (method == "Elizabeth's other ideas") {
+    #implement Elizabeth's other ideas here
   } else {
-    stop("for now, I need the transfer tables!")
-    #decide some way to make a provenance table.
+    stop("unrecognized method")
   }
+    #When time is available, scenarios that aren't relevant to CS - e.g. no hardwood reforestation, no migration, etc
+
 
   provenanceTable[, ecoregionGroup := as.factor(ecoregionGroup)]
   provenanceTable[, Provenance := as.factor(Provenance)]
