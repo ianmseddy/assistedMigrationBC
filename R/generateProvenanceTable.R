92514d4bcf145dda39fb9baad8b26d7ee8ae178c
Ian <ianmseddy@gmail.com>
2020-03-25 08:29:59 -0700

    added BECkey object

--- R/generateProvenanceTable.R
+++ R/generateProvenanceTable.R
@@ -1,43 +1,42 @@
 #' Generate a provenance table for reforestation. This details what species are planted in what ecoregionGroups
 #' and optionally allows for a climate-sensitive genetic modifier
+#' @param ecoregionMap the ecoregion raster
 #' @param transferTable a data.table containing height reductions by seed source and ecoregion. At the moment this is
 #' BECvarfut_plantation, BECvar_seed, and HTp_pred. If not provided, this will return a table where
 #' every species is planted wherever possible, according to ecoregionDT
-#' @param ecoregionCol the column name in ecoregion to match with transferTable - this matching is hardcoded
+#' @param BECkey a data.table that has fields BEC zone/subzone/variants and their corresponding raster values
 #' @param projectedBEC the projected BEC zones. The zones MUST correspond to ecoregionKey and cohortData
-#' @param ecoregionKey a table for mapping ecoregion codes to the BEC zone/subzone/variant
-#' column used by transferTable
 #' @param sppEquiv the LandR sppEquivalency table for matchign species names
 #' @param sppEquivCol the column in sppEquiv by which provenanceTable species should be named
 #' @param method experimental - this will change how particular species are prioritized for planting
 #' @return a provenance table object to be used with LandR_reforestation
 #' @export
-generateBCProvenanceTable <- function(transferTable = NULL, ecoregionCol, ecoregionKey, projectedBEC,
-                                      method = 'default', sppEquiv, sppEquivCol) {
+generateBCProvenanceTable <- function(transferTable, BECkey, projectedBEC, ecoregionMap,
+                                      pixelGroupMap, method = 'default', sppEquiv, sppEquivCol) {
 
   browser()
+  #get sppEquiv species name from transferTable
   transferTable <- copy(transferTable)
+  joinCol <- c('BC_Forestry', eval(sppEquivCol))
+  sppEquivSubset <- sppEquiv[, .SD, .SDcols = joinCol]
 
-  #due to the factorization of the zsv column in BEC zone data,
-  splitUpZones <- strsplit(x = as.character(ecoregionKey[, get(ecoregionCol)]), split = "-")
-  newCol <- lapply(splitUpZones, FUN = function(x){
-    newX <- gsub(x, pattern = "^NA", replacement = "") %>%
-      paste(., collapse = "")
-  }) %>%
-    unlist(.)
-
-  ecoregionKey$newCol <- newCol
-  spp <- c('BC_Forestry', eval(sppEquivCol))
-  sppEquiv <- sppEquiv[, .SD, .SDcols = spp]
-  transferTable <- transferTable[sppEquiv, on = c("species" = "BC_Forestry")]
+  transferTable <- transferTable[sppEquivSubset, on = c("species" = "BC_Forestry")]
   transferTable[, species := NULL]
   setnames(transferTable, eval(sppEquivCol), 'speciesCode')
 
-  setkey(ecoregionKey, newCol)
   setkey(transferTable, BECvarfut_plantation)
 
-  if (method == "default") {
 
+  #figure out what each pixel will become
+  currentAndFutureBECs <- data.table(pixID = 1:ncell(ecoregionMap),
+                                     ecoID = getValues(ecoregionMap),
+                                     projEcoregion = getValues(projectedBEC)) %>%
+    na.omit(.)
+  ecoregionTable <- as.data.table(ecoregionMap@data@attributes[[1]])
+  currentAndFutureBECs <- currentAndFutureBECs[ecoregionMap@data@attributes[[1]], on = c('ecoID' = 'ID')]
+
+  if (method == "default") {
+   transferTable
     #subset each ecoregionGroup/species by the minimum height reduction (ie max when expressed as proportion) among provenances
     optimalProvenance <- transferTable[, score := rank(HTp_pred, ties.method = 'random'), by = .(BECvarfut_plantation, speciesCode)]# %>%
     .[, best := max(score), .(BECvarfut_plantation, speciesCode)] %>%
